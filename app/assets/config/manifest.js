72d88bae85df2f1e61f655d83a1f1d61b23c5cd5
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-11-05 13:02:00 -0500

    Install basic template

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,14 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link sb-admin-2.min.js
+//= link sb-admin-2.min.css
+//= link jquery_easing/jquery.easing.min.js
+//= link undraw_profile.svg
+//= link undraw_profile_1.svg
+//= link undraw_profile_2.svg
+//= link undraw_profile_3.svg
+//= link undraw_rocket.svg
+//= link undraw_posting_photo.svg
+//= link vendor/bootstrap.bundle.min.js
+//= link vendor/jquery.min.js
+//= link font_awesome/all.min.css
