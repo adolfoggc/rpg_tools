11e5fd837f555aee9ce709158909022fc25fdf64
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-12-04 20:05:21 -0500

    Add town sizes enum and it's translations

--- app/models/town_detail.rb
+++ app/models/town_detail.rb
@@ -1,3 +1,14 @@
 class TownDetail < ApplicationRecord
   belongs_to :town
+
+  enum size: {
+    thorp: 1,
+    hamlet: 2, 
+    village: 3,
+    small_town: 4,
+    large_town: 5,
+    small_city: 6,
+    large_city: 7,
+    metropolis: 8
+  }
 end
