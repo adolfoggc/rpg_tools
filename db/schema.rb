399ae6fd7aee4dd6d85cf66548e3f63b2b1a7745
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-12-05 21:53:51 -0500

    Remove test migrations from schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,19 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_024944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "payments", force: :cascade do |t|
-    t.string "pay"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.bigint "town_id", null: false
