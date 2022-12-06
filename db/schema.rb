a0b869835398134d0a4d47118d3fc0979201d431
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-12-05 21:51:38 -0500

    Add shop migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_003341) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_024944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_003341) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.bigint "town_id", null: false
+    t.integer "kind"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["town_id"], name: "index_shops_on_town_id"
   end
 
   create_table "town_details", force: :cascade do |t|
@@ -55,5 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_003341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shops", "towns"
   add_foreign_key "town_details", "towns"
 end
